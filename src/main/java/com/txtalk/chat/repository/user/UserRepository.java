5ad50155e8d480d778c59d907a7e598fd83d2fc5
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-03-05 09:34:33 +0100

    UserRepository extends with JpaRepository

--- src/main/java/com/txtalk/chat/repository/user/UserRepository.java
+++ src/main/java/com/txtalk/chat/repository/user/UserRepository.java
@@ -1,4 +1,11 @@
 package com.txtalk.chat.repository.user;
 
-public class UserRepository {
+import com.txtalk.chat.model.user.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserModel, Integer> {
+
+    Optional<UserModel> findByEmail(String email);
 }
