c5f9760a7ecd7dc9a9aefd86b8f402fd1d31ea2d
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-03-05 09:57:53 +0100

    Updating ReadMe & Adding findByEmail and findAllUsers in UserRepository with nativeQuery

--- src/main/java/com/txtalk/chat/repository/user/UserRepository.java
+++ src/main/java/com/txtalk/chat/repository/user/UserRepository.java
@@ -2,10 +2,15 @@ package com.txtalk.chat.repository.user;
 
 import com.txtalk.chat.model.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
 
     Optional<UserModel> findByEmail(String email);
+
+    @Query(value = "SELECT * FROM users", nativeQuery = true)
+    List<UserModel> findAllUsers();
 }
