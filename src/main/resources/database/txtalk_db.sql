b5811b3e9343765f7edb415d4956a53f02eb3981
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-03-05 13:50:00 +0100

    Updating sql file with tables redone

--- src/main/resources/database/txtalk_db.sql
+++ src/main/resources/database/txtalk_db.sql
@@ -1,6 +1,5 @@
 --@block: create-database
 DROP DATABASE IF EXISTS txtalk_db;
-
 --@block: create-database
 CREATE DATABASE txtalk_db;
 USE txtalk_db;
@@ -13,51 +12,35 @@ CREATE TABLE txtalk_db.user (
   password VARCHAR(255) NOT NULL,
   role VARCHAR(255) NOT NULL
 );
-
 --@block:
 CREATE TABLE txtalk_db.message (
   message_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  sender_id INT NOT NULL,
+  message_content TEXT NOT NULL,
+  user_id INT NOT NULL,
   chat_id INT NOT NULL,
-  message TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES txtalk_db.user(user_id),
+  FOREIGN KEY (chat_id) REFERENCES txtalk_db.chat(chat_id)
 );
-
 --@block:
 CREATE TABLE txtalk_db.chat (
   chat_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  chat_name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- create a join table to join users and messages tables 
 --@block:
-CREATE TABLE txtalk_db.user_message (
-  user_id INT NOT NULL,
-  message_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES user(user_id),
-  FOREIGN KEY (message_id) REFERENCES message(message_id)
-);
-
--- create a join table to join chats and messages tables 
---@block:
-CREATE TABLE txtalk_db.chat_message (
-  chat_id INT NOT NULL,
-  message_id INT NOT NULL,
-  FOREIGN KEY (chat_id) REFERENCES chat(chat_id),
-  FOREIGN KEY (message_id) REFERENCES message(message_id)
-);
+INSERT INTO txtalk_db.user (firstname, lastname, email, password, role)
+VALUES
+('test1', 'NameAdmin', 'test1@example.com', 'pw', 'admin'),
+('test2', 'NameUser', 'test2@example.com', 'pw', 'user'),
+('test3', 'NameUser', 'test3@example.com', 'pw', 'user'),
+('test4', 'NameUser', 'test4@example.com', 'pw', 'user'),
+('test5', 'NameUser', 'test5@example.com', 'pw', 'user');
 
 --@block:
-INSERT INTO txtalk_db.user (firstname, lastname, email, password, role) VALUES
-('John', 'Doe', 'john.doe@example.com', 'password1', 'admin'),
-('Jane', 'Doe', 'jane.doe@example.com', 'password2', 'user'),
-('Alice', 'Smith', 'alice.smith@example.com', 'password3', 'user'),
-('Bob', 'Johnson', 'bob.johnson@example.com', 'password4', 'user'),
-('Charlie', 'Brown', 'charlie.brown@example.com', 'password5', 'admin');
-
+SELECT *
+FROM txtalk_db.user;
 --@block:
-SELECT * FROM txtalk_db.user;
-
---@block:
-SELECT * FROM user u WHERE u.email = "john.doe@example.com";
+SELECT *
+FROM user u
+WHERE u.email = "test1@example.com";
