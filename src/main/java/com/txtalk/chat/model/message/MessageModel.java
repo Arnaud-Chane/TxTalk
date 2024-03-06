d247848cd0148338dde986364326c6c2bcfedcb1
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-03-06 13:59:39 +0100

    Removing the @NamedNativeQuery from MessageModel

--- src/main/java/com/txtalk/chat/model/message/MessageModel.java
+++ src/main/java/com/txtalk/chat/model/message/MessageModel.java
@@ -6,18 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NamedNativeQuery(name = "MessageModel.findMessageDTOByNickname",
-                  query = "SELECT m.message_id,\n" +
-                          "m.message_content,\n" +
-                          "m.created_at,\n" +
-                          "c.chat_name,\n" +
-                          "u.nickname\n" +
-                          "FROM txtalk_db.message m\n" +
-                          "JOIN txtalk_db.user u ON m.user_id = u.user_id\n" +
-                          "JOIN txtalk_db.chat c ON m.chat_id = c.chat_id\n" +
-                          "WHERE u.nickname = ?1",
-                  resultSetMapping = "Mapping.MessageDTO")
-
 @Data
 @Builder
 @NoArgsConstructor
