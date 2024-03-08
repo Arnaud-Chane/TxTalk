8d25f1496d8d9fa31d136caccd8cf14dba7ec0fc
Arnaud <arnaud.chane.dev@gmail.com>
2024-03-08 16:19:42 +0100

    Create RegisterRequestModel

---
+++ src/main/java/com/txtalk/chat/model/register/RegisterRequestModel.java
@@ -0,0 +1,18 @@
+package com.txtalk.chat.model.register;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequestModel {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
