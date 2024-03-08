468a6ceca338a01ea0e152245a46f2f0e588d8b9
Arnaud <arnaud.chane.dev@gmail.com>
2024-03-08 16:11:23 +0100

    Create AuthenticationRequestModel

---
+++ src/main/java/com/txtalk/chat/model/auth/AuthenticationRequestModel.java
@@ -0,0 +1,15 @@
+package com.txtalk.chat.model.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequestModel {
+    private String email;
+    String password;
+}
