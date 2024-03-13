5e4e93381d12b0713c0e46062c16a428ce9df42e
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-03-13 17:50:08 +0100

    Adding Mapping Source dto

--- src/main/java/com/txtalk/chat/service/mapper/message/MessageChatUserMapper.java
+++ src/main/java/com/txtalk/chat/service/mapper/message/MessageChatUserMapper.java
@@ -1,10 +1,19 @@
 package com.txtalk.chat.service.mapper.message;
 
+import com.txtalk.chat.dto.message.MessageDto;
+import com.txtalk.chat.model.entity.chat.ChatModel;
+import com.txtalk.chat.model.entity.message.MessageModel;
+import com.txtalk.chat.model.entity.user.UserModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper
 public interface MessageChatUserMapper {
     @Mapping(source = "MessageModel.message_id", target = "message_id")
+    @Mapping(source = "MessageModel.message_content", target = "message_content")
+    @Mapping(source = "User.nickname", target = "nickname")
+    @Mapping(source = "Chat.chat_name", target = "chat_name")
+    @Mapping(source = "MessageModel.created_at", target = "created_at")
+    MessageDto messageModelToMessageDto(MessageModel MessageModel, UserModel User, ChatModel Chat);
 
 }
